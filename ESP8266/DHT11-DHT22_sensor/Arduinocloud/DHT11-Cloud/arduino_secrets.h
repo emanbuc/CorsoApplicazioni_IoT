7e3ec87350709541e1b4a1057d003d793c925cf6
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2021-12-24 14:09:29 +0100

    fix configurazione connessione

--- ESP8266/DHT11-DHT22_sensor/Arduinocloud/DHT11-Cloud/arduino_secrets.h
+++ ESP8266/DHT11-DHT22_sensor/Arduinocloud/DHT11-Cloud/arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID "stanze-nuove"
-#define SECRET_PASS "aWrC512RTgs"
-#define SECRET_DEVICE_KEY "S1WKERKH7491FDBI5YAD"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define SECRET_DEVICE_KEY ""
